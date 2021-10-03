16512b70854a7e4b5a5c57116342b40c4cc47d36
Cyrill-m <Cyrill-m@mail.ru>
2021-10-03 19:41:32 +0300

    Create Joke service

---
+++ src/main/java/by/mkstudio/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.mkstudio.spring5jokesapp.services;
+//import by.mkstudio.norris.chuck.ChuckNorrisQuotes;
+/**
+ *
+ * @author user
+ */
+public class JokeServiceImpl implements JokeService {
+    
+    //private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    @Override
+    public String getJoke() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
